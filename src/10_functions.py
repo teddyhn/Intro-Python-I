135060e121fae5dd81f50135977470e315e1bb2b
teddyhn <tngo97@gmail.com>
2020-01-13 15:30:32 -0800

    07 ~ 11 complete

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,12 @@
 
 # YOUR CODE HERE
 
+def is_even(num):
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +15,6 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+print(is_even(num))
 
